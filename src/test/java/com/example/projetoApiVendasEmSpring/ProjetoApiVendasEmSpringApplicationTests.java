38d16b18a2fd2eaab018cf6b732749d2dd0558ea
matheusHen <matheushenriquesouzasantos2@gmail.com>
2025-12-12 20:13:56 -0300

    feat: commit com as configurações iniciais

---
+++ src/test/java/com/example/projetoApiVendasEmSpring/ProjetoApiVendasEmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoApiVendasEmSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiVendasEmSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
